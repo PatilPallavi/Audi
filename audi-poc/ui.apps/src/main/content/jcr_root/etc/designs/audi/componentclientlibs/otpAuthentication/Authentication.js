1df90627b6e50a971ec837db1459e2f8a662a147
Pallavi Patil <pallavi.patil4@cognizant.com>
2017-01-23 12:54:25 +0530

    Login component changes

--- audi-poc/ui.apps/src/main/content/jcr_root/etc/designs/audi/componentclientlibs/otpAuthentication/Authentication.js
+++ audi-poc/ui.apps/src/main/content/jcr_root/etc/designs/audi/componentclientlibs/otpAuthentication/Authentication.js
@@ -6,11 +6,9 @@ $(document).ready(function() {
 		var userId, pass;
 
 		if ("undefined" != typeof step) {
-			$(".login").hide();
+			$(".loginsteps").hide();
 			if (step == "loginStep1") {
-
 				$(".login.step2").show();
-
 			} else if (step == "loginStep2") {
 				var check = ValidateEmail($("#email").val());
 				if (check) {
@@ -19,22 +17,17 @@ $(document).ready(function() {
 
 					makeAjaxToServer(userId, pass);
 				}else{
-
 					$(".login.step2").show();
 					$("#invalid_email").show();
-
 				}
-
 			} else if (step == "loginStep3") {
-
 				$(".login.step1").show();
-
-
+				$("#userLabel").find("a").text();
+				$("#userLabel").hide();
+				$("#loginLabel").show();
 			}
-
 		}
 	});
-
 });
 
 function makeAjaxToServer(userId, pass) {
@@ -47,37 +40,32 @@ function makeAjaxToServer(userId, pass) {
 
 		// if received a response from the server
 		success : function(data) {
+			$("#invalid_error").hide();
 			if (data.user) {
 				console.log('success data', data);
 				$(".eingeloggter_user").text(data.user);
 				$(".login.step3").show();
+				$("#loginLabel").hide();
+				$("#userLabel").find("a").text(data.user);
+				$("#userLabel").show();
 				var loginD = [
 				              { 'email' : userId},
 				              { 'name' : data.user }
-
-				              ];
+				             ];
 				document.cookie = "loginD"+ "="+ JSON.stringify(loginD) + ";path=/";
-
-
 			} else {
-
 				$(".login.step2").show();
 				$("#invalid_error").show();
 			}
-
 		},
 
 		// If there was no resonse from the server
 		// #errorMsg is assumption. Need clarification on this
-
 		error : function(textStatus) {
-
 			console.log("Internal API Error " + textStatus);
 			$(".login.step2").show();
 			$("#invalid_error").hide();
-
 		}
-
 	});
 }
 
