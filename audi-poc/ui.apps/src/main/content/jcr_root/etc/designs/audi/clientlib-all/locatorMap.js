b75edb56af938cef6132a59d571db5ef3d378d3d
Pallavi Patil <pallavi.patil4@cognizant.com>
2017-01-27 11:52:55 +0530

    Dealer Locator updates

--- audi-poc/ui.apps/src/main/content/jcr_root/etc/designs/audi/clientlib-all/locatorMap.js
+++ audi-poc/ui.apps/src/main/content/jcr_root/etc/designs/audi/clientlib-all/locatorMap.js
@@ -7,21 +7,26 @@ var hamburgLoc = {
 	lat : 53.5710,
 	lng : 9.9564
   };
+var geocoder;
 function initialize() {
 	var mapCanvas = document.getElementById('map-canvas');
 	var mapOptions = {
 		center : hamburgLoc,
-		zoom : 10
+		zoom : 11,
+        streetViewControl: false,
+        mapTypeControl: false
 	};
 	map = new google.maps.Map(mapCanvas, mapOptions);
 	infowindow = new google.maps.InfoWindow();
 	service = new google.maps.places.PlacesService(map);
+    geocoder = new google.maps.Geocoder();
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			var pos = {
 				lat : position.coords.latitude,
 				lng : position.coords.longitude
 			};
+            getPlaceFromLatLng(pos);
 			searchAudiDealer(pos);
 		}, function() {
 			handleLocationError(true);
@@ -30,7 +35,6 @@ function initialize() {
 		// Browser doesn't support Geolocation
 		handleLocationError(false);
 	}
-	var geocoder = new google.maps.Geocoder();
 	document.getElementById('searchButton').addEventListener('click', function() {
 		geocodeAddress(geocoder);
 	});
@@ -46,7 +50,8 @@ function addMarkerOnMap(place) {
 	var marker = new google.maps.Marker({
 		map : map,
 		position : place.geometry.location,
-		icon: "/content/dam/audi/images/desktop/audi_pin_active.png"
+		icon: "/content/dam/audi/images/desktop/audi_pin_active.png",
+        title: place.name
 	});
 	google.maps.event.addListener(marker, 'click', function() {
 		var infoHtml = "<p><span style='font-weight: 600;'>" + place.name + "</span></p><p>" + place.formatted_address + "</p>"
@@ -66,12 +71,14 @@ function removeMarkers() {
 }
 
 function handleLocationError(browserHasGeolocation) {
+    getPlaceFromLatLng(hamburgLoc);
 	searchAudiDealer(hamburgLoc);
 	console.log(browserHasGeolocation ? 'Error: The Geolocation is not allowed.'
 					: 'Error: Your browser doesn\'t support geolocation.');
 }
 
 function geocodeAddress(geocoder) {
+    $('#mapErrorMessage').hide();
 	var address = document.getElementById('searchLocation').value;
 	geocoder.geocode({
 		'address' : address
@@ -80,6 +87,7 @@ function geocodeAddress(geocoder) {
 			searchAudiDealer(results[0].geometry.location);
 		} else {
 			console.log('Geocode was not successful for the following reason: ' + status);
+			$('#mapErrorMessage').show();
 		}
 	});
 }
@@ -101,4 +109,18 @@ function callback(results, status) {
 			addMarkerOnMap(results[i]);
 		}
 	}
+}
+
+function getPlaceFromLatLng(location) {
+    geocoder.geocode({'location': location}, function(results, status) {
+        if (status === 'OK') {
+            if (results[3]) {
+                document.getElementById('searchLocation').value = results[3].formatted_address;
+            } else {
+                console.log('No results found for reverse geocoder');
+            }
+        } else {
+            console.log('Reverse Geocoder failed due to: ' + status);
+        }
+    });
 }
